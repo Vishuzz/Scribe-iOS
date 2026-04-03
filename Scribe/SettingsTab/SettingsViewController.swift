32017ce52da350aae041d1b7fc9bcc313dd67d5f
Vishek Tyagi <tyagivishek1234@gmail.com>
2026-04-03 15:48:16 +0530

    Created LanguageSettingsView.swift which handles layout toggles and translation source selection.

--- Scribe/SettingsTab/SettingsViewController.swift
+++ Scribe/SettingsTab/SettingsViewController.swift
@@ -206,56 +206,10 @@ extension SettingsViewController: UITableViewDelegate {
             }
 
         case .specificLang:
-            if let viewController = storyboard?.instantiateViewController(
-                identifier: "TableViewTemplateViewController"
-            ) as? TableViewTemplateViewController {
-                // Copy base settings.
-                var data = SettingsTableData.languageSettingsData
-                // Check if the device is an iPad to remove the Layout Section.
-                if DeviceType.isPad {
-                    for menuOption in data[1].section {
-                        if menuOption.sectionState == .none(.toggleAccentCharacters)
-                            || menuOption.sectionState == .none(.toggleCommaAndPeriod) {
-                            data[1].section.remove(at: 0)
-                        }
-                    }
-                    if data[1].section.isEmpty {
-                        data.remove(at: 1)
-                    }
-                } else {
-                    // Languages where we can disable accent keys.
-                    let accentKeyLanguages: [String] = [
-                        languagesStringDict["German"]!,
-                        languagesStringDict["Spanish"]!,
-                        languagesStringDict["Swedish"]!
-                    ]
-
-                    let accentKeyOptionIndex =
-                        SettingsTableData.languageSettingsData[1].section.firstIndex(where: { s in
-                            s.sectionTitle.elementsEqual(
-                                NSLocalizedString(
-                                    "i18n.app.settings.keyboard.layout.disable_accent_characters",
-                                    value: "Disable accent characters", comment: ""
-                                )
-                            )
-                        }) ?? -1
-
-                    // If there are no accent keys we can remove the `Disable accent characters` option.
-                    if accentKeyLanguages.firstIndex(of: section.sectionTitle) == nil,
-                       accentKeyOptionIndex != -1 {
-                        data[1].section.remove(at: accentKeyOptionIndex)
-                    } else if accentKeyLanguages.firstIndex(of: section.sectionTitle) != nil,
-                              accentKeyOptionIndex == -1 {
-                        data[1].section.insert(
-                            SettingsTableData.languageSettingsData[2].section[2], at: 1
-                        )
-                    }
-                }
-
-                viewController.configureTable(for: data, parentSection: section)
-
-                navigationController?.pushViewController(viewController, animated: true)
-            }
+            let hostingController = UIHostingController(
+                rootView: LanguageSettingsView(parentSection: section)
+            )
+            navigationController?.pushViewController(hostingController, animated: true)
 
         default: break
         }
